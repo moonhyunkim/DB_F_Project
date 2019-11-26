9bb2b44146a35f8843f68d7697d6e496270fda6b
MoonHyun Kim <moonhyun@MoonHyuns-MacBook-Pro.local>
2019-11-26 19:33:13 +0900

    init configuration for Develop

--- DBP/python/DBconnecting.py
+++ DBP/python/DBconnecting.py
@@ -1,8 +1,9 @@
 import pymysql.cursors
 
-conn = pymysql.connect(host='192.168.56.103', port=4567 ,user='project_user',password='Sjrnfl1!2!',db='madang')
+#conn = pymysql.connect(host='192.168.56.103', port=4567 ,user='project_user',password='Sjrnfl1!2!',db='madang')
+conn = pymysql.connect(host='localhost', port=3306, user='root', password ='sjrnfl12', db='ATMProject')
 cur = conn.cursor()
-sql = 'select * from Book'
+sql = 'Show tables'
 cur.execute(sql)
 res = cur.fetchall()
 print(res)
